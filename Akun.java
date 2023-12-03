ad2754bcf1867bb439993edc8557b47eba45bbc2
dndamaharani <133856114+dndamaharani@users.noreply.github.com>
2023-12-03 22:21:30 +0700

    Create Akun.java

---
+++ Akun.java
@@ -0,0 +1,10 @@
+/**
+ * Kelas Akun merepresentasikan entitas akun dalam sistem.
+ * Setiap akun memiliki sebuah identifikasi unik yang disebut dengan ID.
+ */
+class Akun {
+    /**
+     * Variabel instance untuk menyimpan ID dari akun.
+     */
+    public String id;
+}
