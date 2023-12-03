1c60dfbfbc61c01dc810ca7735a63be341d58476
Dayat <149884842+Dayatnur@users.noreply.github.com>
2023-12-03 22:10:40 +0700

    Create Bank.java

---
+++ Bank.java
@@ -0,0 +1,19 @@
+/**
+ * Kelas {@code Bank} merupakan turunan dari kelas {@code Pembayaran} dan
+ * merepresentasikan metode pembayaran
+ * menggunakan layanan perbankan.
+ */
+public class Bank extends Pembayaran {
+
+    /**
+     * Memeriksa apakah pembayaran menggunakan layanan perbankan berhasil.
+     *
+     * @return {@code true} jika pembayaran berhasil; {@code false} jika pembayaran
+     *         tidak berhasil.
+     */
+    public boolean isPembayaranBerhasil() {
+        // Implementasi logika untuk Bank
+        return true; // Nilai sementara, seharusnya sesuai dengan logika implementasi yang
+                     // sebenarnya.
+    }
+}
